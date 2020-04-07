0537805d9b95c955fa8829ed0c79cb1d20440d9d
shoebjoarder <shoebjoarder@outlook.com>
2020-04-07 18:39:11 +0200

    added unique student id

--- src/studentdatabaseapp/Student.java
+++ src/studentdatabaseapp/Student.java
@@ -6,10 +6,11 @@ public class Student {
 	private String firstName;
 	private String lastName;
 	private int gradeYear;
-	private int studentID;
+	private String studentID;
 	private String courses;
 	private int tuitionBalance;
-	private int costOfCourse = 600;
+	private static int costOfCourse = 600;
+	private static int id = 1000;
 	
 	// Constructor: prompt user to enter student's name and year
 	public Student() {
@@ -23,11 +24,17 @@ public class Student {
 		System.out.println("1 - Freshmen\n2 - Sophmore\n3 - Junior\n4 - Senior\nEnter student class level: ");
 		this.gradeYear = in.nextInt();
 		
-		System.out.println(firstName + " " + lastName + " " + gradeYear);
+		setStudentID();
 		
+		System.out.println(firstName + " " + lastName + " " + gradeYear + " " + studentID);
 	}
 	
 	// Generate an ID
+	private void setStudentID() {
+		// Grade level + static ID
+		id++;
+		this.studentID = gradeYear + "" + id;
+	}
 	
 	
 	// Enroll in courses
