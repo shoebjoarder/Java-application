1fc1d2cbaa4f2b767321476cf9e7998a4580b4d6
shoebjoarder <shoebjoarder@outlook.com>
2020-04-07 21:08:30 +0200

    Added for loop, ask number of students

--- src/studentdatabaseapp/StudentDatabaseApp.java
+++ src/studentdatabaseapp/StudentDatabaseApp.java
@@ -1,15 +1,23 @@
 package studentdatabaseapp;
 
+import java.util.Scanner;
+
 public class StudentDatabaseApp {
 
 	public static void main(String[] args) {
-		Student stud1 = new Student();
 		// Ask how many users needed to be added
-		stud1.enroll();
-		stud1.payTuition();
-		System.out.println(stud1.showInfo());
+		System.out.print("Enter number of students you want to enroll: ");
+		Scanner in = new Scanner(System.in);
+		int numberOfStudents = in.nextInt();
+		Student[] student = new Student[numberOfStudents];
+			
 		// Create n number of new students
-		
+		for(int n=0; n<numberOfStudents; n++) {
+			student[n] = new Student();
+			student[n].enroll();
+			student[n].payTuition();
+			System.out.println(student[n].showInfo());
+		}	
 	}
 
 }
