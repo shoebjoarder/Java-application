17a6c4f89b1c18fd74f94badf934b129c433f72a
shoebjoarder <shoebjoarder@outlook.com>
2020-04-07 18:25:08 +0200

    Added the outline

---
+++ src/studentdatabaseapp/Student.java
@@ -0,0 +1,28 @@
+package studentdatabaseapp;
+
+public class Student {
+	private String firstName;
+	private String lastName;
+	private String gradeYear;
+	private int studentID;
+	private String courses;
+	private int tuitionBalance;
+	private int costOfCourse = 600;
+	
+	// Constructor: prompt user to enter student's name and year
+	
+	
+	// Generate an ID
+	
+	
+	// Enroll in courses
+	
+	
+	// View balance 
+	
+	
+	// Pay tuition
+	
+	
+	// Show status
+}
